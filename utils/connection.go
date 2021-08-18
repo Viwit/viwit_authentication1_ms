3b44a50a5ea74f1fe8cc37b12d63f868faceceef
Jairo Suarez <andres4005@gmail.com>
2021-08-17 20:50:05 -0500

    update db credentials

--- utils/connection.go
+++ utils/connection.go
@@ -7,7 +7,7 @@ import (
 )
 
 func GetConnection() *gorm.DB {
-	dsn := "root:jbj4cNRqd7NWnMd@tcp(authentication.cjareiirr0dz.us-east-1.rds.amazonaws.com:3306)/viwit?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "ikHV5b7fXF:sub54mrAjY@tcp(remotemysql.com:3306)/ikHV5b7fXF?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
